f5feb60b85678dd1b8f8ace3c82327c47e30896d
miguelalegrez <miguelalegrez@gmail.com>
2024-03-31 19:33:42 +0200

    header
    Cambios en el header principal

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  imports: [RouterOutlet, RouterLink],
+  styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent {
-
-}
+export class HeaderComponent {}
